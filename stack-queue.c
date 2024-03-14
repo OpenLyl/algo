51dc8c820afa19c94cb3f8ef7d1a4dd29a0377e3
MetaSky <creator_meta_sky@163.com>
2024-03-15 00:35:29 +0800

    add link queue basic struct.

--- stack-queue.c
+++ stack-queue.c
@@ -83,7 +83,7 @@ void linkStackDestroy(LinkStack *link_stack) {
 
 void testLinkStack() {
     LinkStack *link_stack = linkStackCreate();
-    
+
     for (int i = 0; i < 4; i++) {
         int res = linkStackPush(link_stack, i);
         if (res < 0) {
@@ -106,13 +106,126 @@ void testLinkStack() {
     linkStackDestroy(link_stack);
 }
 
-void testLinkQueue() {
+// define
+// create
+// enqueue
+// dequeue
+// dump
+// destroy
 
+typedef struct LinkQueueNode {
+    int data;
+    struct LinkQueueNode *next;
+} LinkQueueNode;
+
+typedef struct LinkQueue {
+    LinkQueueNode *head;
+    LinkQueueNode *tail;
+} LinkQueue;
+
+#define LINK_QUEUE_IS_EMPTY(link_queue) (link_queue->head == NULL)
+
+LinkQueue *linkQueueCreate() {
+    LinkQueue *link_queue = (LinkQueue *)malloc(sizeof(LinkQueue));
+    if (link_queue == NULL) {
+        return NULL;
+    }
+
+    link_queue->head = NULL;
+    link_queue->tail = NULL;
+
+    return link_queue;
+}
+
+int linkQueueEnqueue(LinkQueue *link_queue, int value) {
+    LinkQueueNode *node = (LinkQueueNode *)malloc(sizeof(LinkQueueNode));
+    if (node == NULL) {
+        return -1;
+    }
+    node->data = value;
+    node->next = NULL;
+
+    if (link_queue->head == NULL) {
+        link_queue->head = node;
+    } else {
+        link_queue->tail->next = node;
+    }
+    link_queue->tail = node;
+
+    return 0;
+}
+
+int linkQueueDequeue(LinkQueue *link_queue, int *value) {
+    if (LINK_QUEUE_IS_EMPTY(link_queue)) {
+        return -1;
+    }
+
+    LinkQueueNode *head = link_queue->head;
+    *value = head->data;
+    link_queue->head = head->next;
+    if (link_queue->head == NULL) {
+        link_queue->tail = NULL;
+    }
+    free(head);
+
+    return 0;
+}
+
+void linkQueueDump(LinkQueue *link_queue) {
+    LinkQueueNode *node = link_queue->head;
+    int index = 0;
+    while (node) {
+        printf("link queue items[%d] = %d\n", index, node->data);
+        node = node->next;
+        index++;
+    }
+    printf("\r\n");
+} 
+
+void linkQueueDestroy(LinkQueue *link_queue) {
+    if (link_queue == NULL) {
+        return;
+    }
+
+    int value;
+    while (!LINK_QUEUE_IS_EMPTY(link_queue)) {
+        linkQueueDequeue(link_queue, &value);
+    }
+    free(link_queue);
+}
+
+void testLinkQueue() {
+    LinkQueue *link_queue = linkQueueCreate();
+
+    for (int i = 0; i < 5; i++) {
+        int res = linkQueueEnqueue(link_queue, i);
+        if (res < 0) {
+            printf("link queue enqueue failed\n");
+        }
+    }
+    linkQueueDump(link_queue);
+
+    int value;
+    for (int i = 0; i < 6; i++) {
+        int res = linkQueueDequeue(link_queue, &value);
+        if (res < 0) {
+            printf("link queue dequeue failed\n");
+        } else {
+            printf("link queue dequeue value = %d\n", value);
+        }
+    }
+
+    int res = linkQueueEnqueue(link_queue, 100);
+    if (res < 0) {
+        printf("link queue enqueue 100 failed\n");
+    }
+    linkQueueDump(link_queue);
+
+    linkQueueDestroy(link_queue);
 }
 
 int main() {
-    printf("stack and queue\n");
-    testLinkStack();
-    // testLinkQueue();
+    // testLinkStack();
+    testLinkQueue();
     return 0;
 }
